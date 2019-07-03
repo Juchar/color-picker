b347ef4a69660fa585f0ea4d8522a1ba50c4867d
Juchar <Julien.Charpenel@Gmail.com>
2019-07-03 13:07:18 +0200

    Automate gh pages generation

---
+++ gh-pages.bat
@@ -0,0 +1,18 @@
+@echo off
+
+SET ghPagesBranch=gh-pages-new
+echo Using branch %ghPagesBranch%
+
+git branch -D %ghPagesBranch%
+git push origin --delete %ghPagesBranch%
+git checkout -b %ghPagesBranch% master
+
+bower install
+
+git rm -r --cached .
+DEL .gitignore
+REN .gitignore-gh-pages .gitignore
+git add .
+
+git commit -m "Create GitHub Pages" --no-verify
+git push origin %ghPagesBranch%
