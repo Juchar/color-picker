fb0f29e04d18e375b182c1d59cb50666fe3c6e52
Juchar <Julien.Charpenel@Gmail.com>
2019-07-03 16:41:34 +0200

    Automate GitHub pages deployment using travis

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,29 +1,53 @@
-#!/bin/bash -e
+# This script pushes a demo-friendly version of your element and its
+# dependencies to gh-pages.
 
-ghPagesBranch=gh-pages
-echo "Using branch $ghPagesBranch"
+# usage gp Polymer core-item [branch]
+# Run in a clean directory passing in a GitHub org and repo name
 
-git clone https://Juchar:${GITHUB_TOKEN}@github.com/Juchar/color-picker.git --branch=master gh-pages
-cd gh-pages
-git remote rm origin
-git remote add origin https://Juchar:${GITHUB_TOKEN}@github.com/Juchar/color-picker.git
+GH_REF="github.com/${TRAVIS_REPO_SLUG}"
 
-git branch -D $ghPagesBranch
-git push origin --delete $ghPagesBranch
-git checkout -b $ghPagesBranch master
+org=`echo ${TRAVIS_REPO_SLUG} | cut -f 1 -d /`
+repo=`echo ${TRAVIS_REPO_SLUG} | cut -f 2 -d /`
 
-git rm -r --cached .
+name="Travis CI"
+email="Juchar@Github.com"
+branch=${3:-"master"} # default to master when branch isn't specified
 
-bower cache clean
-bower install --force
-bower prune
+mkdir temp && cd temp
 
-polymer analyze > analysis.json
+# make folder (same as input, no checking!)
+mkdir $repo
+git clone "https://${GITHUB_TOKEN}@${GH_REF}" --single-branch
 
-rm .gitignore
-mv .gitignore-gh-pages .gitignore
+# switch to gh-pages branch
+pushd $repo >/dev/null
+git checkout --orphan gh-pages
 
-git add .
+# remove all content
+git rm -rf -q .
 
-git commit -m "Create GitHub Pages" --no-verify
-git push origin $ghPagesBranch --force
+# use bower to install runtime deployment
+bower cache clean $repo # ensure we're getting the latest from the desired branch.
+git show ${branch}:bower.json > bower.json
+echo "{
+  \"directory\": \"components\"
+}
+" > .bowerrc
+bower install
+bower install $org/$repo#$branch
+git checkout ${branch} -- demo
+rm -rf components/$repo/demo
+mv demo components/$repo/
+
+# redirect by default to the component folder
+echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
+
+git config user.name $name
+git config user.email $email
+
+# send it all to github
+git add -A .
+git commit -am 'Deploy to GitHub Pages'
+git push --force --quiet -u "https://${GITHUB_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+
+popd >/dev/null
