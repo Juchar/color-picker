e0216e351ccc80b63c8b92b55525b8a760e36a5c
Juchar <Julien.Charpenel@Gmail.com>
2019-07-03 16:18:40 +0200

    Automate GitHub pages deployment using travis

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,6 +3,11 @@
 ghPagesBranch=gh-pages
 echo "Using branch $ghPagesBranch"
 
+git clone https://Juchar:${GITHUB_TOKEN}@github.com/Juchar/color-picker.git --branch=master gh-pages
+cd gh-pages
+git remote rm origin
+git remote add origin https://Juchar:${GITHUB_TOKEN}@github.com/Juchar/color-picker.git
+
 git branch -D $ghPagesBranch
 git push origin --delete $ghPagesBranch
 git checkout -b $ghPagesBranch master
